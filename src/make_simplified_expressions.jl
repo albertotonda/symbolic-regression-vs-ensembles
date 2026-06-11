fec2fd0da1006047bef6020235295d0f80642286
Alberto Tonda <alberto.tonda@gmail.com>
2026-06-11 16:36:05 +0200

    Update make_simplified_expressions.jl

--- src/make_simplified_expressions.jl
+++ src/make_simplified_expressions.jl
@@ -58,76 +58,82 @@ end
 # and directly access the SymPy expressions without having to bother with parsing
 
 # hard-coded values
+folds = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+folds = [0]
 task_id = 361258
-fold_id = 0
 results_directory = "results_server_hyperparameter_tuning_20260519/"
-pickled_pysr_file = results_directory * "PySRRegressor_task_$(task_id)_fold_$(fold_id).pkl"
+output_directory = "latex/"
+output_file = output_directory * "simplified_julia_equations_task_$(task_id).tex"
 
 # import the necessary Python packages
 pickle = pyimport("pickle")
+sympy_printing = pyimport("sympy.printing.julia")
 
-println("Reading \"" * pickled_pysr_file * "\"...")
-pysrregressor = pickle.loads(read(pickled_pysr_file))
+# start from an empty string
+latex_string = ""
 
-selected_expression_file_name = results_directory * "PySRRegressor_validation_equation_task_$(task_id)_fold_$(fold_id).tex"
-println("Reading .tex file with the selected expression \"" * selected_expression_file_name * "\"...")
-selected_latex = read(selected_expression_file_name, String)
-println(selected_latex)
+for fold_id in folds
 
-# we can iterate over the equations, looking for the one which was selected by the validation process
-for (index, row) in enumerate(pysrregressor.equations_.itertuples())
-    # convert fields to variable types usable in Julia
-    comp = pyconvert(Int, row.complexity)
-    loss = pyconvert(Float64, row.loss)
+    pickled_pysr_file = results_directory * "PySRRegressor_task_$(task_id)_fold_$(fold_id).pkl"
 
-    # get the sympy expression
-    println("Now analyzing the equation with complexity $comp and loss $loss...")
+    println("---")
+    println("Reading \"" * pickled_pysr_file * "\"...")
+    pysrregressor = pickle.loads(read(pickled_pysr_file))
 
-    # let's try to understand whether the equation is actually the same as the one in the file
-    current_latex = pyconvert(String, pysrregressor.latex(index-1))
-    if current_latex == selected_latex
-        println("- Found it!")
+    selected_expression_file_name = results_directory * "PySRRegressor_validation_equation_task_$(task_id)_fold_$(fold_id).tex"
+    println("Reading .tex file with the selected expression \"" * selected_expression_file_name * "\"...")
+    selected_latex = read(selected_expression_file_name, String)
+    println(selected_latex)
+
+    # we can iterate over the equations, looking for the one which was selected by the validation process
+    selected_pyexpr = nothing
+    selected_equation_string = nothing
+    for (index, row) in enumerate(pysrregressor.equations_.itertuples())
+        # convert fields to variable types usable in Julia
+        comp = pyconvert(Int, row.complexity)
+        loss = pyconvert(Float64, row.loss)
+
+        # get the sympy expression
+        println("Now analyzing the equation with complexity $comp and loss $loss...")
+
+        # let's try to understand whether the equation is actually the same as the one in the file
+        current_latex = pyconvert(String, pysrregressor.latex(index-1))
+        if current_latex == selected_latex
+            println("- Found it!")
+            selected_pyexpr = row.sympy_format
+            selected_equation_string = pyconvert(String, row.equation)
+        end
+
+    end # end loop over rows
+
+    # now that we have the expression, let's work with it
+    for py_sym in selected_pyexpr.free_symbols
+        var_name = Symbol(pyconvert(String, py_sym.name))
+        Core.eval(@__MODULE__, :(@syms $var_name))
     end
 
-    sympy_expr = row.sympy_format
+    julia_str = pyconvert(String, sympy_printing.julia_code(selected_pyexpr))
 
+    julia_expr = Meta.parse(julia_str)
+    sym_utils_tree = Core.eval(@__MODULE__, julia_expr)
+
+    simplified = SymbolicUtils.simplify(sym_utils_tree)
+
+    println("Original: ", selected_equation_string)
+    println("Simplified: ", simplified)
+    println("---")
+
+    # convert the simplified string to LaTeX; need an intermediate
+    # conversion using SymbolicUtils.to_expr() to avoid issues
+    simplified_latex = latexify(SymbolicUtils.Code.toexpr(simplified), env=:raw)
+
+    # add this to the LaTeX block
+    global latex_string *= raw"\begin{align}" * "\n"
+    global latex_string *= "y = " * simplified_latex * "\n"
+    global latex_string *= raw"\end{align}" * "\n"
 end
 
-
-# ==============================================================================
-# Pipeline Execution
-# ==============================================================================
-# 1. Define the path to your input text/TeX file
-#file_path = "results_server_hyperparameter_tuning_20260519/PySRRegressor_validation_equation_task_361258_fold_0.tex"
-
-# 2. Read the entire file content as a raw string
-#latex_input = read(file_path, String)
-
-# Input messy LaTeX string
-#latex_input = raw"\frac{\sin^2(x) + \cos^2(x)}{\exp(y) \cdot \exp(-y)} + \log(x)"
-
-#println("Processing expression...")
-#sym_expr = parse_latex_to_symbolicutils(latex_input)
-
-# Simplify using SymbolicUtils engine
-#simplified_expr = SymbolicUtils.simplify(sym_expr)
-#println("Simplified Expression: ", simplified_expr)
-
-# ==============================================================================
-# New Step: Convert Back to LaTeX and Save to File
-# ==============================================================================
-
-# 1. Convert the symbolic object to a LaTeX string using Latexify
-# env=:raw strips out any outer equation or math block environments (e.g., \[ ... \])
-#latex_output_str = latexify(simplified_expr, env=:raw)
-
-# 2. Define target text file path
-#output_filename = "simplified_equation.tex"
-
-# 3. Open file and write the raw LaTeX string
-#open(output_filename, "w") do file
-#    write(file, latex_output_str)
-#end
-
-#println("Success! Saved LaTeX output to: $output_filename")
-#println("Raw file contents: ", latex_output_str)
+# finally, write the latex to file
+open(output_file, "w") do file
+    write(file, latex_string)
+end
